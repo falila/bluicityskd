dc801636cd4657b0a4a89decc17046ee3d475581
raphael <soulfa23@gmail.com>
2020-07-15 22:17:04 -0400

    timer

--- src/blu/db_wrapper.py
+++ src/blu/db_wrapper.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy import Column, String ,Integer
 from sqlalchemy.ext.declarative import declarative_base  
 from sqlalchemy.orm import sessionmaker
+from contextlib import contextmanager
 Base = declarative_base()
 
 class Tracker(Base):  
@@ -25,16 +26,31 @@ class Wrapper():
     def __init__(self,db_string):
         self.db = create_engine(db_string)  
         self.Base = Base
-        Session = sessionmaker(self.db)  
-        self.session = Session()
+        #Session = sessionmaker(self.db)  
+        #self.session = Session()
         self.Base.metadata.create_all(self.db)
+    
+    
 
-    def add_records(self, trackers_list=None, commit=True):
-        for tr in trackers_list:
-            self.session.add(tr)  
-        
-        if commit:
-            self.session.commit()
+    @contextmanager
+    def session_scope(self):
+        """Provide a transactional scope around add records."""
+        Session = sessionmaker(self.db) 
+        session = Session()
+        try:
+            yield session
+            session.commit()
+        except:
+            session.rollback()
+            raise
+        finally:
+            session.close()
+
+    def add_records(self, trackers_list=None):
+
+        with self.session_scope() as session:
+            for tr in trackers_list:
+                session.add(tr)  
 
 if __name__ == "__main__":
     # for testing
